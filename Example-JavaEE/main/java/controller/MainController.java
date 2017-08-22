3e1a4c19ef02582f2e8647bec6f3c669d107d973
Pomeo44 <for_vkontakte86@mail.ru>
2017-08-22 09:12:44 +0300

    work's continuing

--- Example-JavaEE/main/java/controller/MainController.java
+++ Example-JavaEE/main/java/controller/MainController.java
@@ -1,12 +1,10 @@
 package controller;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
-public class MainController extends Application {
+@Path(value = "/")
+public class MainController {
 
 
 	@GET
