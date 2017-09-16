70c9d973eb9cb6fb65cd9f0b4fc2f6b86b47c42f
aantipin <aantipin@mmtr.ru>
2017-09-16 16:22:10 +0300

    work's continuing

--- demo/src/main/java/org/pomeo44/configuration/WebConfiguration.java
+++ demo/src/main/java/org/pomeo44/configuration/WebConfiguration.java
@@ -1,14 +1,27 @@
 package org.pomeo44.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
-//@Configuration
+@Configuration
 public class WebConfiguration extends WebMvcConfigurationSupport {
 
-//    @Override
-//    public void configureViewResolvers(ViewResolverRegistry registry) {
-//        registry.jsp("/templates", ".jsp");
-//    }
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        registry.viewResolver(resolver);
+    }
+
 }
